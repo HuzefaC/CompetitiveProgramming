63548629c589aed1b124dbcbbb038220e3833397
HuzefaC <huzefachabukswar@gmail.com>
2021-01-20 20:38:45 +0530

    Completed DominoPiling

---
+++ CodeForces/ProblemSet/DominoPiling/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int m,n;
+  int domino_area = 2*1;
+  cin >> m >> n;
+  cout << (m * n) / domino_area;
+}
